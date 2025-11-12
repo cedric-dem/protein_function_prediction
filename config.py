f1a3a1795e51966bcbc08a7ee9509f2e74304b82
cedric <demuelenaere.cedric@gmail.com>
2025-11-12 17:27:48 +0100

    fix config

--- config.py
+++ config.py
@@ -6,9 +6,9 @@ KNN_MODEL_NAME = "model_v7_knn.pkl"
 
 SUBMISSION_NAME = "model_v7.tsv"
 
-STRATEGY = "KNN"
-HIDDEN_LAYER = None
-EXPONENT_OCCURENCES = 2 #TODO try with 3
+STRATEGY = "NN"
+HIDDEN_LAYER = 2000
+EXPONENT_OCCURENCES =  3
 
 positions = ['A', "B", 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', "U", 'V', 'W', "X", 'Y', "Z"]
 # positions = ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y']
